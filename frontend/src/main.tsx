af253c5fa792bf1781917b4d781bf5ab639e4088
Igor Holt <iholt@mymail.aacc.edu>
2026-02-24 14:06:19 -0500

    Add Vite React cybersecurity dashboard frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BlackhorseKGraphDashboard from './BlackhorseKGraph';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BlackhorseKGraphDashboard />
+  </React.StrictMode>
+);
